61f75f6ea857e0a60253e7923d1bf8e245714bb3
Yashaswini-11 <yashudn10@gmail.com>
2026-06-14 17:18:11 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
